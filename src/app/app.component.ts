55cc78718e9a2d1079052a4317dc969f32d35b01
HabibSebGJ <habibsebastiangj@gmail.com>
2024-08-13 20:27:41 -0500

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  act: {Title:string; Description: string; Submit: boolean}[] = [];
+
+  addList(Title:string, Description: string, Submit: boolean){
+this.act.push({Title, Description, Submit})
+  }
   constructor() {}
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { tasklist } from 'src/app/home/Task.list';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,10 +10,34 @@ public Title: string= '';
 public Description: string= '';
 public Sub: boolean= false;
 public Done: boolean= false;
-public submis: Array<{Title:string, Description: string}> =[];
-
+list: tasklist[] = [];
 constructor() {}
+
+addlist(){
+if(this.Title.length >=3 && this.Description.length >=10){
+  const newlist: tasklist={
+    Title: this.Title,
+    Description: this.Description,
+    Submit: false,
+  }
+this.list.push(newlist)
+this.Reset();
+console.log('', this.list);
+}
+}
+Reset(){
+  this.Title='';
+  this.Description='';
+}
+
 ValidRequirement(){
   this.Sub= this.Title.length>=3 && this.Description.length >=10;
 }
+
+marASdone(task:tasklist){
+const index = this.list.indexOf(task);
+if(index !== 1){
+this.list[index].Submit = true;
+    }
+  }
 }
